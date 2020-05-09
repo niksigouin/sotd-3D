8bfab9c5efe47a3d381f26be9e63b1d594d9bdd7
Nikolas Sigouin <nik.sigouin@gmail.com>
2020-05-09 16:04:14 -0400

    Updated NetworkManager and LobbyScene

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -1,18 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
-public class NetworkManager : MonoBehaviour
+public class NetworkManager : MonoBehaviourPunCallbacks
 {
+    [Header("Connection Status")]
+    public Text connectionStatusText;
+
+    [Header("Login UI Panel")]
+    public GameObject Login_UI_Panel;
+    public InputField playerNameInput; // Player name Input (Change to TMPro?)
+    
+
+    // PANELS
+    [Header("Game Options UI Panel")]
+    public GameObject GameOptions_UI_Panel;
+
+    [Header("Create Room UI Panel")]
+    public GameObject CreateRoom_UI_Panel;
+
+    [Header("Inside Room UI Panel")]
+    public GameObject InsideRoom_UI_Panel;
+
+    [Header("Room List UI Panel")]
+    public GameObject RoomList_UI_Panel;
+
+    [Header("Join Random Room UI Panel")]
+    public GameObject JoinRandomRoom_UI_Panel;
+
+    #region Unity Methods
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ActivatePanel(Login_UI_Panel.name);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        connectionStatusText.text = "Connection status: " + PhotonNetwork.NetworkClientState;
     }
+
+    #endregion
+
+    #region UI Callbacks
+
+    public void OnLoginButtonClicked()
+    {
+        string playerName = playerNameInput.text;
+        if (!string.IsNullOrEmpty(playerName))
+        {
+            PhotonNetwork.LocalPlayer.NickName = playerName;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        else
+        {
+            // WARN USER THAT THE NAME DOES NOT MEET STANDARDS
+            Debug.Log("Playername is invalid");
+        }
+    }
+
+    #endregion
+
+    #region Photon Callbacks
+    public override void OnConnected()
+    {
+        Debug.Log("Connected to Internet");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + " is connected to Photon");
+        ActivatePanel(GameOptions_UI_Panel.name);
+    }
+
+
+    #endregion
+
+    #region Public Methods
+    public void ActivatePanel(string panelToBeActivated)
+    {
+        Login_UI_Panel.SetActive(panelToBeActivated.Equals(Login_UI_Panel.name));
+        GameOptions_UI_Panel.SetActive(panelToBeActivated.Equals(GameOptions_UI_Panel.name));
+        CreateRoom_UI_Panel.SetActive(panelToBeActivated.Equals(CreateRoom_UI_Panel.name));
+        InsideRoom_UI_Panel.SetActive(panelToBeActivated.Equals(InsideRoom_UI_Panel.name));
+        RoomList_UI_Panel.SetActive(panelToBeActivated.Equals(RoomList_UI_Panel.name));
+        JoinRandomRoom_UI_Panel.SetActive(panelToBeActivated.Equals(JoinRandomRoom_UI_Panel.name));
+
+
+    }
+
+
+    #endregion
 }
