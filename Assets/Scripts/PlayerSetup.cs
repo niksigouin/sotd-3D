2caddc69b6694b7d9a9acadc1a7f11c9e0628eee
Nikolas Sigouin <nik.sigouin@gmail.com>
2020-05-10 00:38:49 -0400

    Mouvement Synchronisation

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSetup : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    GameObject playerCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (photonView.IsMine )
+        {
+            transform.GetComponent<MouvementController>().enabled = true;
+            playerCamera.GetComponent<Camera>().enabled = true;
+        }
+        else
+        {
+            transform.GetComponent<MouvementController>().enabled = false;
+            playerCamera.GetComponent<Camera>().enabled = false;
+        }
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
