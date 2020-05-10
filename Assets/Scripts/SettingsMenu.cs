ca0d94abd1df739c834cd943dc79d9b02bb09108
Nikolas Sigouin <nik.sigouin@gmail.com>
2020-05-10 19:03:08 -0400

    Added Volume Option

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [Header("Audio")]
+    public AudioMixer mainMixer;
+
+    #region Public Methods
+
+    public void SetVolume(float volume)
+    {
+        mainMixer.SetFloat("MasterVolume", volume);
+    }
+
+    #endregion
+}
