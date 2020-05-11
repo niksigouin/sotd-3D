a61b5ffd8567237f0d9b565503382503ff34f321
Nikolas Sigouin <nik.sigouin@gmail.com>
2020-05-11 02:53:50 -0400

    Added Cursor lock and fixed In Game menu

---
+++ Assets/Scripts/GameSetup.cs
@@ -0,0 +1,32 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSetup : MonoBehaviour
+{
+    public static GameSetup GS;
+
+    public void OnEnable()
+    {
+        if (GameSetup.GS == null)
+        {
+            GameSetup.GS = this;
+        }
+    }
+
+    public void DisconnectPlayer()
+    {
+        StartCoroutine(DisconnectAndLoad());
+    }
+
+    IEnumerator DisconnectAndLoad()
+    {
+        PhotonNetwork.Disconnect();
+        while (PhotonNetwork.IsConnected)
+            yield return null;
+
+        SceneManager.LoadScene(0);
+    }
+}
